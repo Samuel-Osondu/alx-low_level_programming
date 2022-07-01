fb7216941bc7c796a5c6e949ffe0144ddcf910d8
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-01 12:52:23 -0700

    want to become a fullstack engineer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	float d;
 	char c;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(c));
-	printf("size of an int: %lu byte(s)\n", sizeof(i));
-	printf("size of a long int: %lu byte(s)\n", sizeof(l));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("size of a float: %lu byte(s)\n", sizeof(d));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
