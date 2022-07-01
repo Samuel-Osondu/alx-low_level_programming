53176a30272dee703b949aba8982763eae2e159f
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-01 06:51:14 -0700

    want to be a fullstack engineer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
