a3981a5c8e7f2833332cb87471a2102ed9a62f00
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-01 08:47:01 -0700

    want to be a fullstack engineer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 }
-
 	int i;
 	long int l;
 	long long int ll;
@@ -18,5 +17,4 @@ int main(void)
 	printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
 	printf("size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
-
 }
