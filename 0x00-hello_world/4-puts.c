8be6c9fe77c95b07b1d740c82f8723bd12d21f9e
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-06-30 21:18:37 -0700

    want to be a fullstack eengineer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-#include <stdio.h> #include <string.h>
+#include <stdio.h> #include <string.h> /** * main - Entry point * * Return: Always 0 (Success) */ int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
