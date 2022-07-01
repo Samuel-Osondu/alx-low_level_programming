f2dad619033e59cc3722e6527c9f902cf73b7fbe
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-01 02:26:52 -0700

    want to be a fullstack engineer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string>
 
 /**
- *  main - Entry point
- *  Return: Always  0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
-{ 
- printf("with proper grammer, but the outcome is a piece of art,\n");
- return (0);
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
