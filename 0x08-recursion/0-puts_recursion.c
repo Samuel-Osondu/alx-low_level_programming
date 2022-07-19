a295b78bddc9c67f75f966e0ad65a761bdf4f761
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-19 10:42:02 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * _putchar - prints the string
+ * @s: char to check
+ *
+ * Return: 0 is success
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+# 0x08. C - Recursion
+1. Write a function that prints a string, followed by a new line.
