1692e8df8a974e92e6735512407a89f2d6c6aab8
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-20 01:38:28 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _alcuadrado - give me the square root of a
+ * _esprimo - shows me when a number is prime
  *
- * @n: the given number
- * @corre: from 0 up looking for square always less than the half of the number
- * Return: if does not have square or corre if it find it
+ * @n: int to check
+ *
+ * @corre: int to check
+ * Return: 0 if is not a prime
  */
-int _alcuadrado(int corre, int n)
+int _esprimo(int n, int corre)
 {
-	if (corre < 0)
-		return (-1);
-	if (corre == 1)
+	if (n < 2)
+		return (0);
+	if (n == 2)
 		return (1);
-	if (n > corre / 2)
-		return (-1);
-	if (corre != n * n)
-		return (_alcuadrado(corre, n + 1))
-	if (corre == n * n)
-		return (n);
-	return (-1);
+	if (corre >= n / 2)
+		return (1);
+	if (n % corre == 0)
+		return (0);
+	else
+		return (_esprimo(n, corre + 1));
+	return (1);
 }
+
 /**
- * _sqrt_recursion - return de square root of a natural number
+ * is_prime_number - function return 1 if is prime number
  *
  * @n: to check
  *
- * Return: the square root or -1 if it does not have
+ * Return: 0 if is not prime number
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-	return (_alcuadrado(n, 0));
+	return (_esprimo(n, 2));
 }
