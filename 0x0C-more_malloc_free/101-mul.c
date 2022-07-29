91723fbdd9d3dfd8d52f357d5f1c596fd4798b5f
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-28 18:01:56 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,11 +5,12 @@
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
+ *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
-	int i = 0;
+int i = 0;
 
 	while (s[i])
 	{
