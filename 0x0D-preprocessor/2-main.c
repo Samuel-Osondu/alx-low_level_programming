9c104515471025befd4d0ca62339fd6793c110bc
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-07-31 04:06:42 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints the name of the current file
+ *
+ * Return: 0 after succesful run
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+	return (0);
+}
