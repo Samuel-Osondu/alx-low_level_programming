e2fc4d57b1dfe6484bbac047507d06d1d4cf4ced
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-08-01 00:22:55 -0700

    Poppy

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c)
+g(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
+
