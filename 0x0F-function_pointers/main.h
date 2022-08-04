9e46865ca1d94a8369f80c2d97ccf43bfd82bbe6
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-08-03 22:55:43 -0700

    What's my name

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,8 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
