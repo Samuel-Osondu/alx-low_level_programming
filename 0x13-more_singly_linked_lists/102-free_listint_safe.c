42959870e2ba37dd99c72af0d3d99a313db820eb
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-08-17 09:03:03 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,57 +1,39 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
- * Return: address of the node where the loop starts, or NULL
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ * Return: number of elements in the freed list
  */
-listint_t *find_listint_loop(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-		listint_t *slow = head;
+	if (!h || !*h)
+		return (0);
 
-			listint_t *fast = head;
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
 
+	*h = NULL;
 
-
-				if (!head)
-
-							return (NULL);
-
-
-
-					while (slow && fast && fast->next)
-
-							{
-
-										fast = fast->next->next;
-
-												slow = slow->next;
-
-														if (fast == slow)
-
-																	{
-
-																					slow = head;
-
-																								while (slow != fast)
-
-																												{
-
-																																	slow = slow->next;
-
-																																					fast = fast->next;
-
-																																								}
-
-																											return (fast);
-
-																													}
-
-															}
-
-
-
-						return (NULL);
-
+	return (len);
 }
