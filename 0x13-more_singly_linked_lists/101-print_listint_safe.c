a356f5bb47aa374a12477adf259d3a56c04679a3
Samuel-Osondu <osondusamuelchemist75@gmail.com>
2022-08-17 07:24:41 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,6 @@
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
- *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
